787010df0c56822c345c12db42f24622520e6afe
franciscosalceek1 <salceekmunozfrancisco@gmail.com>
2025-02-25 17:18:50 -0300

    actuañlizacion

--- Pygame/Main_copia_p.py
+++ Pygame/Main_copia_p.py
@@ -17,7 +17,7 @@ from Logica.Consola_1_MP import *
 # si tengo tiempo y ganas, implementar el submenu musical
 # si tengo tiempo y ganas, implementar diccionarios de donde sacar datos
 
-
+#pedirle a ochoa que me de una devolucion del juego
 
 
 pygame.init() 
